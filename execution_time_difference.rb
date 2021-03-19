95769443db4e7c2f04802af0de7e7e5460e999bb
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-19 14:04:35 -0400

    solved phase 1 of largest contiguous subsum

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -25,21 +25,39 @@
 # p my_min2(list)  # =>  -5
 list = [5, 3, -7]
 
+#   # possible sub-sums
+#   [5]           # => 5
+#   [5, 3]        # => 8 --> we want this one
+#   [5, 3, -7]    # => 1
+#   [3]           # => 3
+#   [3, -7]       # => -4
+#   [-7]          # => -7
+
 def largest_contiguous_subsum(arr)
     max = arr[0]
     new_arr = []
+    # p new_arr
     (0...arr.length).each do |idx1|
-        (0...arr.length).each do |idx2|
+        (idx1...arr.length).each do |idx2|
             if idx1 == idx2
-                new_arr << arr[idx1]
-                sum = arr[idx1] + arr[idx2]
-                if sum > max
-                    max = sum
-                end
-            end
+                new_arr << [arr[idx1]]
+            else
+                new_arr << arr[idx1..idx2]
+            end           
         end
     end
+
+    new_arr.each do |subArray|    #[[5], [[5, 3]], [[5, 3, -7]], [3], [[3, -7]], [-7]]
+        sum = subArray.sum
+        if sum > max
+            max = sum
+        end        
+    end
+
     max
 end
 p largest_contiguous_subsum(list) # => 8
+# [[5], [5, 3], [5, -7], [3, -7]]
+p largest_contiguous_subsum([5]) # => 8
+p largest_contiguous_subsum([5, 1, 3]) # => 9
 
