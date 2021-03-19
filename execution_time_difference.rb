a7a066c9ab342393b358df343d7850bbf2549142
kihs <ajkihs@gmail.com>
2021-03-19 12:30:40 -0400

    Implement sluggish my_min

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -1,3 +1,14 @@
-def my_min(arr)
-    
-end
+ list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+ 
+ def my_min(arr)    # runs in O(n^2)
+    min = arr.first
+    arr.each do |ele1|
+        arr.each do |ele2|
+            if ele1 < ele2 && ele1 < min
+                min = ele1
+            end
+        end
+    end
+    min
+ end
+ p my_min(list)  # =>  -5
