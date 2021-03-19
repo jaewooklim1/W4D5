b62f550b90111fb72f710b992374680ff80b0891
kihs <ajkihs@gmail.com>
2021-03-19 15:33:44 -0400

    Finish all anagrams

--- Anagrams.rb
+++ Anagrams.rb
@@ -2,8 +2,7 @@
 # This means that the letters in one word can be rearranged to form the other word. 
 # For example:
 
-def first_anagram?(string1, string2)
-
+def first_anagram?(string1, string2)  # space/time o(n!)
     string1.chars.to_a.permutation.map(&:join).include?(string2)
 
 end
@@ -11,7 +10,7 @@ end
 # p second_anagram?("gizmo", "sally")    #=> false
 # p second_anagram?("elvis", "lives")    #=> true
 
-def second_anagram?(string1, string2)
+def second_anagram?(string1, string2)   #o(n^2)
 
     splitString2 = string2.split("")
     string1.each_char do |char|
@@ -24,5 +23,36 @@ def second_anagram?(string1, string2)
 end
 
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+
+def third_anagram?(string1, string2)    # Ruby uses
+    string1.split("").sort == string2.split("").sort
+end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+def fourth_anagram?(string1, string2)
+    hash1 = Hash.new(0)
+    hash2 = Hash.new(0)
+    string1.split("").each {|char1| hash1[char1]+=1}
+    string2.split("").each {|char2| hash2[char2]+=1}
+
+    hash1 == hash2
+end
+
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
+
+def fifth_anagram?(string1, string2)
+    count = Hash.new(0)
+    string1.split("").each {|char1| count[char1]+=1}
+    string2.split("").each {|char2| count[char2]-=1}
+    count.all? {|k,v| v.zero?}
+end
+
+# p fifth_anagram?("gizmo", "sally")    #=> false
+# p fifth_anagram?("elvis", "lives")    #=> true
+
