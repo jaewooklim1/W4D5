027ce7d4da113bb1f55c0c5fd4f242b416b33fcb
kihs <ajkihs@gmail.com>
2021-03-19 13:41:44 -0400

    Begin largestr_contiguous_subsum method

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -11,4 +11,35 @@
     end
     min
  end
- p my_min(list)  # =>  -5
+
+ def my_min2(arr)   # runs in O(n)
+    arr.inject do |ele, accum| 
+        if ele<accum
+            ele
+        else
+            accum
+        end
+    end
+ end
+# p my_min(list)  # =>  -5
+# p my_min2(list)  # =>  -5
+list = [5, 3, -7]
+
+def largest_contiguous_subsum(arr)
+    max = arr[0]
+    new_arr = []
+    (0...arr.length).each do |idx1|
+        (0...arr.length).each do |idx2|
+            if idx1 == idx2
+                new_arr << arr[idx1]
+                sum = arr[idx1] + arr[idx2]
+                if sum > max
+                    max = sum
+                end
+            end
+        end
+    end
+    max
+end
+p largest_contiguous_subsum(list) # => 8
+
